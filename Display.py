ae7bef2ddc5639f0ebcae782d71a83901e4a628d
Christian Madajski <cmad7317@tutanota.com>
2021-12-08 07:55:46 -0500

    TaskTracker - added command list with descriptions to the help menu

--- Display.py
+++ Display.py
@@ -1,4 +1,12 @@
 
 
 def helpMenu():
-    print("Help menu goes here")
+    print("LIST OF COMMON COMMANDS:")
+    print("ADD - adds a new task for the day")
+    print("ADD [taskName] - adds a new task for the day")
+    print("RM - removes the most recently made task")
+    print("RM [taskIndex] - removes task associated with the given index")
+    print("LS - list all tasks for the current day")
+    print("COMP - mark/unmark a task as completed")
+    print("COMP [taskIndex] - mark/unmark a task as completed")
+    print("EXIT - terminate the program")
