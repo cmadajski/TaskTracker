6f5807c8d23d9ed66e6eaba14783812e8803c2f3
Christian Madajski <cmad7317@tutanota.com>
2021-12-30 07:33:46 -0500

    TaskTracker - added help menus for the ADD and RM commands

--- Display.py
+++ Display.py
@@ -1,14 +1,22 @@
 
 
 def helpMenu():
-    print("LIST OF COMMON COMMANDS:")
-    print("ACC - shows current account being used for task tracking")
-    print("LOGIN - allows current user to switch to a different account")
-    print("ADD - adds a new task for the day")
-    print("ADD [taskName] - adds a new task for the day")
-    print("RM - removes the most recently made task")
-    print("RM [taskIndex] - removes task associated with the given index")
-    print("LS - list all tasks for the current day")
-    print("COMP - mark/unmark a task as completed")
-    print("COMP [taskIndex] - mark/unmark a task at a specific index as completed")
-    print("EXIT - terminate the program")
+    print("List of Common Commands:")
+    print("  USER - shows current user information")
+    print("  ADD - adds a new task for the current day")
+    print("  RM - removes a task from the current day")
+    print("  LS - list all tasks for the current day")
+    print("  COMP - mark/unmark a task as completed")
+    print("  EXIT - terminate the program")
+
+def add_help():
+    print('Help for ADD command')
+    print('  ADD - asks user to provide a new task name before creating new task')
+    print('  ADD [taskname] - adds a new task with the provided name')
+    print('  ADD HELP - prints the help menu for the ADD command')
+
+def rm_help():
+    print('Help for RM command')
+    print('  RM - removes the most recently made task from the current day')
+    print('  RM [taskindex] - removes the task at the given index value for the current day')
+    print('  RM HELP - prints the help menu for the RM command')
